e09b84207f9e7af455652ad6b68fd01e3ed1023a
Stephane Boucaud <stephane.boucaud@outlook.fr>
2019-12-13 04:53:39 +0100

    added tqdm functionnality

--- InceptionV3/train.py
+++ InceptionV3/train.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+from tqdm import tqdm
+
 print("PyTorch Version: ",torch.__version__)
 print("Torchvision Version: ",torchvision.__version__)
 
@@ -68,7 +70,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
             running_corrects = 0
 
             # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
+            for inputs, labels in tqdm(dataloaders[phase]):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
